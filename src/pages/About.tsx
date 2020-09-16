cb7772b94d7ab7eb2331abc2f720e2d4b9bb7281
derrmru <podogomacbookair@MacBook-Air.local>
2020-09-16 16:18:34 +0200

    About content update

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -13,7 +13,7 @@ const About: React.FC = () => {
             <Menu />
             <div className="about-text">
                 <h1>How To Split Tips</h1>
-                    <p>There is no one way of splitting tips fairly amongst staff.</p>
+                    <p>There is no single way of splitting tips fairly amongst staff.</p>
                     <p>The two most common methods are:</p>
                     <ul>
                         <li>Splitting by Hours Worked</li>
@@ -21,14 +21,14 @@ const About: React.FC = () => {
                     </ul>
 
                 <h2>Splitting By Hours</h2>
-                    <p>Many restaurants split tips by hours worked. This is fairer, so that people working shorter shifts but at better times don't get more tips than others.</p>
+                    <p>Many restaurants and bars split tips by hours worked. This is fairer, so that people working shorter shifts but at better times don't get more tips than others.</p>
                     <p>This involves dividing the total tips received by total hours worked, then multiplying this by the hours each individual has worked.</p>
                     <a href="/split-by-hours">
                         <strong>| Split My Tips By Hours</strong>
                     </a>
 
                 <h2>Splitting Tips Amongst Support Staff</h2>
-                    <p>Some restaurants choose to split their tip pool (all of the tips) amongst all of their staff. This means that all supporting staff get a fair share, and no-one will suffer from having a particularly bad shift.</p>
+                    <p>Some restaurants or bars choose to split their tip pool (all of the tips) amongst all of their staff. This means that all supporting staff get a fair share, and no-one will suffer from having a particularly bad shift.</p>
                     <p>Most managers will split the pool amongst different staff categories, e.g.</p>
                     <ul>
                         <li>Bar Staff: 30%</li>
